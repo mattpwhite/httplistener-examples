3076bed63fb9a243bc1dbcea8fb939d53b585a1f
Matt White <whitema@deshaw.com>
2018-02-06 21:12:21 -0500

    Add .NET Core test

---
+++ TestHttpListenerNetCore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestHttpListenerNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Provide URL as argument");
+                Environment.Exit(1);
+            }
+
+            var url = args[0];
+            Console.WriteLine($"Testing {url}");
+
+            using (var httpClient = new HttpClient(new HttpClientHandler { UseDefaultCredentials = true }))
+            {
+                try
+                {
+                    Console.WriteLine($"\nTesting HttpClient");
+                    using (var result = httpClient.GetAsync(url).GetAwaiter().GetResult())
+                    {
+                        Console.WriteLine($"Status: {result.StatusCode}");
+                        Console.WriteLine($"WWW-Authenticate: {result.Headers.WwwAuthenticate}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error - {ex}");
+                }
+            }
+        }
+    }
+}
